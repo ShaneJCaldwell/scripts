bfbebbcfd1594a6c523d384899c7a0450dac6567
scald <scald@red.dhcp.baker>
2017-11-14 18:06:52 -0800

    comments, make bash

--- bash/manual_filtering/copy_files_score_range.sh
+++ bash/manual_filtering/copy_files_score_range.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# This script will take a pool of pdb files and the scorefile that they had generated and based upon a value, only copy if the
-# model fell within a specific range default lower limit is zero, but could be higher if binning, other things possible.
+# S. Caldwell 2017.11.14
+
+# This script will take a pool of pdb files and the scorefile 
+# that they had generated and based upon a value, only copy if the
+# model fell within a specific range default lower limit is zero, 
+# but could be higher if binning, other things possible.
 
 # Set the thresholds, upper and lower
 upper=300
@@ -10,7 +14,7 @@ directory=/path/to/directory
 
 # Send every line in scorefile - starting at 3 - to awk, make i the score of interest (in this case fa_rep of a default scorefunction)
 # and last (pdb filename) space-delimited items
-for i in `tail -n +3 ../score.sc | awk '{print $9 "," $NF}'`
+for i in $(tail -n +3 ../score.sc | awk '{print $9 "," $NF}')
 do 
 	#fa_rep is column 9 in this file. read it and filter based on cutoff
 	score=$(echo $i | awk -F"," '{print $1}')

43a26feab98faecf20562b33725f8845cc38f6d2
scald <scald@red.dhcp.baker>
2017-11-15 15:43:41 -0800

    draw a map with electron density map at different colours in a spectrum rainbow

---
+++ pymol/pymol_scripts/densityrainbows.py
@@ -0,0 +1,22 @@
+
+clip slab, 2.0
+
+isomesh meshmin3, dens, -3.0
+isomesh meshmin2, dens, -2.0
+isomesh meshmin1, dens, -1.0
+isomesh mesh0, dens, 0.0
+isomesh mesh1, dens, 1.0
+isomesh mesh2, dens, 2.0
+isomesh mesh3, dens, 3.0
+isomesh mesh4, dens, 4.0
+isomesh mesh5, dens, 5.0
+
+color red, mesh5
+color tv_red, mesh4
+color orange, mesh3
+color yelloworange, mesh2
+color yellow, mesh1
+color limon, mesh0
+color green, meshmin1
+color greencyan, meshmin2
+color skyblue, meshmin3

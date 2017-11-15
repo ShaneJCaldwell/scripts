429c4f6e75e114a71c784c93897d0240ee198e65
scald <scald@red.dhcp.baker>
2017-11-14 18:10:19 -0800

    comments

--- bash/grep/grep_reindex_ids.sh
+++ bash/grep/grep_reindex_ids.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# S Caldwell 2017.11.14
+
+# Use this script to copy lines from an origin file to a new file
+# Originally used to pull lines from a reindexing log into a new log as the 
+# pdb files the index was based on was filtered down.
+
 # For every file in a folder with given (eg. pdb) extension
 for i in $(ls *.pdb)
 do 
